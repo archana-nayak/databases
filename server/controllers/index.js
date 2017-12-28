7b39f5ddc5f439fb94f963a114c4923c687e6519
archana <archana.nayak@gmail.com>
2017-12-27 19:15:47 -0800

    changes querying for request parameters to strings

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,22 +1,26 @@
 var models = require('../models');
 
+// var userFields = ['username'];
+
 module.exports = {
   messages: {
     get: function (req, res) {
     // a function which handles a get request for all messages
       models.messages.get(function(err, messages) {
+        console.log('in controller, request', messages);
         res.json(messages);
       });
     },
     // a function which handles posting a message to the database 
     post: function (req, res) {
-      var params = [req.body[text], 
-        req.body[username], req.body[roomname]];
-      models.messages.post(params, function(err, result) {
+      console.log('in controller ', req.body);
+      var params = [req.body['text'], 
+        req.body['username'], req.body['roomname']];
+      models.messages.post(params, function(err, results) {
         if (err) {
           throw err;
         }
-        res.json(201);
+        res.json(results);
       });
     } 
   },
@@ -24,12 +28,13 @@ module.exports = {
   users: {
     // Ditto as above
     get: function (req, res) {
-      models.users.get(function(err, users) {
-        res.json(users);//send back as json through express
+      models.users.get(function(err, results) {
+        console.log('in controller users ', results);
+        res.json(results);//send back as json through express
       });
     },
     post: function (req, res) {
-      var params = [req.body[username]];
+      var params = [req.body['username']];
       models.users.post(params, function(err, results) {
         res.json(results);
       });
