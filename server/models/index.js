510afb42f9d72d613dc69475dfa15823bec31b94
archana <archana.nayak@gmail.com>
2017-12-27 17:35:38 -0800

    Insert to and Query from database  using express without ORM

--- server/models/index.js
+++ server/models/index.js
@@ -4,28 +4,28 @@ module.exports = {
   messages: {
 
     get: function (callback) {
-    	// a function which produces all the messages
-    	var queryString = 'select messages.id, messages.text, messages.roomname,\
-    	 users.username from messages left outer join users on \
-    	 messages.userid = users.id order by messages.id desc';
-    	db.query(queryString,
-    	  function(err, result) {
-    	  	if (err) {
-    	  		throw err;
-    	  	}
-    			callback(result);
-    	});
+      // a function which produces all the messages
+      var queryString = 'select messages.id, messages.text, messages.roomname,\
+       users.username from messages left outer join users on \
+       messages.userid = users.id order by messages.id desc';
+      db.query(queryString,
+        function(err, result) {
+          if (err) {
+            throw err;
+          }
+          callback(result);
+        });
     }, 
     post: function (params, callback) {
-    	// a function which can be used to insert a message into the database
-    	var queryString = 'insert into messages (text, userid,\
-    	 roomname) values (?, (select id from users where username = ? limit 1), ?)';
-    	db.query(queryString, params, function(err, result) {
-    	 	if (err) {
-    	 	  throw err;
-    	 	}
-    	  callback(result);
-    	});
+      // a function which can be used to insert a message into the database
+      var queryString = 'insert into messages (text, userid,\
+       roomname) values (?, (select id from users where username = ? limit 1), ?)';
+      db.query(queryString, params, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        callback(result);
+      });
     } 
   },
 
@@ -33,19 +33,19 @@ module.exports = {
     // Ditto as above.
 
     get: function (callback) {
-    	//get all users from the users table
-    	var queryString = 'select username from users';
-    	db.query(queryString, function(err, result) {
-    		callback(result);
-    	});
+      //get all users from the users table
+      var queryString = 'select username from users';
+      db.query(queryString, function(err, result) {
+        callback(result);
+      });
     },
     //pass an array of parameter values for the insert
     post: function (params, callback) {
-    	//add a user into the users table
-    	var queryString = 'insert into users (username) values (?)';
-    	db.query(queryString, params, function(err, result) {
-    			callback(result);
-    	});
+      //add a user into the users table
+      var queryString = 'insert into users (username) values (?)';
+      db.query(queryString, params, function(err, result) {
+        callback(result);
+      });
     }
   }
 };
