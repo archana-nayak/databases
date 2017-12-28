c08a4ada8608467d2db6bc792ce04da3ea5819dc
archana <archana.nayak@gmail.com>
2017-12-27 17:35:13 -0800

    Insert to and Query from database  using express without ORM

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,51 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+
+    get: function (callback) {
+    	// a function which produces all the messages
+    	var queryString = 'select messages.id, messages.text, messages.roomname,\
+    	 users.username from messages left outer join users on \
+    	 messages.userid = users.id order by messages.id desc';
+    	db.query(queryString,
+    	  function(err, result) {
+    	  	if (err) {
+    	  		throw err;
+    	  	}
+    			callback(result);
+    	});
+    }, 
+    post: function (params, callback) {
+    	// a function which can be used to insert a message into the database
+    	var queryString = 'insert into messages (text, userid,\
+    	 roomname) values (?, (select id from users where username = ? limit 1), ?)';
+    	db.query(queryString, params, function(err, result) {
+    	 	if (err) {
+    	 	  throw err;
+    	 	}
+    	  callback(result);
+    	});
+    } 
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+
+    get: function (callback) {
+    	//get all users from the users table
+    	var queryString = 'select username from users';
+    	db.query(queryString, function(err, result) {
+    		callback(result);
+    	});
+    },
+    //pass an array of parameter values for the insert
+    post: function (params, callback) {
+    	//add a user into the users table
+    	var queryString = 'insert into users (username) values (?)';
+    	db.query(queryString, params, function(err, result) {
+    			callback(result);
+    	});
+    }
   }
 };
 
