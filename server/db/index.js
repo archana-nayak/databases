ff874e34c40b7514be3f1d578031c424d11f8015
archana <archana.nayak@gmail.com>
2017-12-27 17:36:48 -0800

    Create connection object and connect to chat database

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,20 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+module.exports.connection = mysql.createConnection(
+  {
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'chat'
+  });
+
+connection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to database ');
+});
+
 
 
