6330d8787d1e34b7a14368bad7b824b74d7e83d7
archana <archana.nayak@gmail.com>
2017-12-27 17:37:32 -0800

    table schema for chat database

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,17 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  text VARCHAR(200) NOT NULL,
+  userid INTEGER(11) NOT NULL,
+  roomname VARCHAR(20) 
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users ( 
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(40) NOT NULL
+);
 
 
 
