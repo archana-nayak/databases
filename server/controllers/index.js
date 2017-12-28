5adce86ae0a55f4d2db7e18c646346d3d70ece9e
archana <archana.nayak@gmail.com>
2017-12-27 17:31:26 -0800

    Post and Get data using express without ORM

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,38 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+    	// a function which handles a get request for all messages
+			models.messages.get(function(err, messages) {
+    		res.json(messages);
+			});
+    },
+    // a function which handles posting a message to the database 
+    post: function (req, res) {
+    	var params = [req.body[text], 
+    	req.body[username], req.body[roomname]];
+    	models.messages.post(params, function(err, result) {
+    		if (err) {
+    			throw err;
+    		}
+    		res.json(201);
+    	});
+    } 
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+    	models.users.get(function(err, users) {
+    		res.json(users);//send back as json through express
+    	});
+    },
+    post: function (req, res) {
+    	var params = [req.body[username]];
+    	models.users.post(params, function(err, results) {
+    		res.json(results);
+    	});
+    }
   }
 };
 
